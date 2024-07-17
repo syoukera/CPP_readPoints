6f3be63502adc87fdb18fffa3a5488c977b04626
syoukera <s.akira2986@gmail.com>
2024-07-17 11:00:45 +0900

    [compile] change compilar to icpx

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-g++ read_points.cpp interpolate.cpp writeData.cpp 
+# g++ read_points.cpp interpolate.cpp writeData.cpp -o read_point_gxx
+icpx read_points.cpp interpolate.cpp writeData.cpp -o read_point_icpx
