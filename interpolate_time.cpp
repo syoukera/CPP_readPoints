a6e10cbb82954b7d58cafac774da7deef961e48c
syoukera <s.akira2986@gmail.com>
2024-07-30 14:55:19 +0900

    load lower and upper reference UData

--- interpolate_time.cpp
+++ interpolate_time.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "data_point.h"
 using namespace std;
+
+// ファイルからデータを読み込む関数
+bool readDataFromFile(const string& filename, vector<DataPoint>& data) {
+    ifstream infile(filename);
+    if (!infile) {
+        cerr << "ファイルを開けませんでした: " << filename << endl;
+        return false;
+    }
+
+    int num_points;
+    string line;
+    double x, y, z;
+
+    // 最初の行をスキップ（ファイルフォーマットによる）
+    getline(infile, line);
+
+    // ファイルからデータを読み取り
+    infile >> num_points; // 最初の数値はデータの数として処理
+    infile.ignore(); // 改行を読み飛ばす
+
+    // 各行を読み取り、sscanfで解析
+
+    while (getline(infile, line)) {
+
+        // sscanfで解析
+        if (sscanf(line.c_str(), "(%le %le %le)", &x, &y, &z) == 3) {
+            data.push_back({x, y, z});
+        } else {
+            // cerr << " : 解析エラー" << endl;
+        }
+    }
+
+    // ファイルを閉じる
+    infile.close();
+    return true;
+}
 
 // list_timesファイルに記載されたすべてのファイル名をベクターに読み込む関数
 bool readListOfFiles(const string& listFilename, vector<string>& filenames) {
@@ -21,6 +58,28 @@ bool readListOfFiles(const string& listFilename, vector<string>& filenames) {
 }
 
 int interpolateUbyTime(double interpTime, string& lowerReference, string& upperReference) {
+    
+    // 読み込む流速データのパス
+    string lowerReferenceUFilename = "/NAS/18/NH3_HiTAC/fuel_Xinterp/" + lowerReference + "/U";
+    string upperReferenceUFilename = "/NAS/18/NH3_HiTAC/fuel_Xinterp/" + upperReference + "/U";
+    
+    // 読み込む流速データのvectorを定義する
+    vector<DataPoint> lowerReferenceUData, upperReferenceUData;
+
+    // "U"ファイルからデータを読み込む
+    if (!readDataFromFile(lowerReferenceUFilename, lowerReferenceUData)) {
+        cerr << "データを読み込めませんでした: " << lowerReferenceUFilename << endl;
+        return 1;
+    }
+    if (!readDataFromFile(upperReferenceUFilename, upperReferenceUData)) {
+        cerr << "データを読み込めませんでした: " << upperReferenceUFilename << endl;
+        return 1;
+    }
+
+    for (size_t i = 0; i < lowerReferenceUData.size(); ++i) {
+        cout << lowerReferenceUData[i].x << " " << upperReferenceUData[i].x << endl;
+    }
+
     return 0;
 }
 
@@ -85,5 +144,9 @@ int main() {
                 break;
             }
         }
+
+    // for development only
+    break;
+
     }
 }
