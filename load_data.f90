a594d5d258a65d90a6db995613e8bc4c4fecf99e
syoukera <s.akira2986@gmail.com>
2024-08-19 11:02:50 +0900

    [load_data] imprement asign of gridData

--- load_data.f90
+++ load_data.f90
@@ -1,13 +1,35 @@
 program read_vectors
     implicit none
-    integer :: n, i, j
-    real :: x, y, z
+    integer :: n, i, j, k
+    double precision :: x, y, z
     character(len=100) :: line
     character(len=1) :: paren
 
     integer, parameter :: grid_size = 101
     integer, parameter :: total_grid_size = grid_size*grid_size
-    real, dimension(grid_size, grid_size, 3) :: gridData
+    double precision, dimension(grid_size, grid_size, 3) :: gridData
+
+    ! parameter of FK3 grid
+    integer, parameter :: nx = 2000, ny = 1100, nz = 1, ibd = 4, jbd = 4, kbd = 4
+
+    integer, parameter :: jinlet_center = ny/2.0;
+    integer, parameter :: jinlet_sta = jinlet_center - (grid_size - 1)/2.0
+    integer, parameter :: jinlet_end = jinlet_center + (grid_size - 1)/2.0
+    integer :: jinlet
+    integer, parameter :: kinlet = (grid_size - 1)/2.0 + 1 ! fix reference point of z index on import velocity as center line
+
+    ! assume no mpi
+    integer, parameter :: ista = 1, iend = nx
+    integer, parameter :: jsta = 1, jend = ny
+    integer, parameter :: ksta = 1, kend = nz
+
+    double precision, parameter :: u1 = 1.0 ! air velocity [cm/s]
+
+    double precision, allocatable, save :: u(:,:,:), v(:,:,:), w(:,:,:)
+
+    allocate (u(ista-ibd:iend+ibd,jsta-jbd:jend+jbd,ksta-kbd:kend+kbd))
+    allocate (v(ista-ibd:iend+ibd,jsta-jbd:jend+jbd,ksta-kbd:kend+kbd))
+    allocate (w(ista-ibd:iend+ibd,jsta-jbd:jend+jbd,ksta-kbd:kend+kbd))
     
     ! Open the file
     open(unit=10, file='output/0.0000000/U', status='old', action='read')
@@ -41,7 +63,43 @@ program read_vectors
 
         end do
     end do
-    
+
+
+    ! Asign velocities
+    ! for loop among z and y range 
+    do k = ksta - kbd, kend + kbd
+        do j = jsta - jbd, jend + jbd
+
+            ! for loop inside on upper boundery of x
+            do i = ista, ista - ibd , -1
+
+                ! calclate first row, and later copy it
+                if (i == ista) then
+                    
+                    ! check if y is insile of inlet
+                    if (jinlet_sta <= j .and. j <= jinlet_end) then
+
+                        ! get j-index as origin to be inlet start
+                        jinlet = j - (jinlet_sta - 1)
+
+                        ! grid data is (z, y, x) cordinate
+                        u(i, j, k) = gridData(kinlet, jinlet, 3) ! z-direction of import velocity
+                        v(i, j, k) = gridData(kinlet, jinlet, 2) ! y-direction of import velocity
+                        w(i, j, k) = gridData(kinlet, jinlet, 1) ! x-direction of import velocity
+
+                    else
+                        ! set surrounding velocity
+                        u(i, j, k) = u1
+                    endif
+                else 
+                    ! set same velocity as first row
+                    u(i, j, k) = u(ista, j, k)
+                endif
+
+            end do
+        end do
+    end do
+
     ! Close the file
     close(10)
     
